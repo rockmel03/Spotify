4ed4edf6c5c44123fe1f3c413b1e72f0ddf469f3
rockmel03 <kamalswag2@gmail.com>
2023-09-24 16:49:07 +0530

    second Commit

--- script.js
+++ script.js
@@ -64,7 +64,10 @@ const previousBtn = document.querySelector('#masterPrivious')
 const nextBtn = document.querySelector('#masterNext')
 const seekBar = document.querySelector('#seek-bar')
 const playlistplayBtn = document.querySelector('.playlist-buttons > i')
+const loopBtn = document.querySelector('#loopBtn')
 
+
+// making list of song in html 
 
 trackContainer.innerHTML = ''
 songs.forEach((song, index) => {
@@ -77,52 +80,69 @@ songs.forEach((song, index) => {
     </div>`
 })
 
-
 let songIndex = 0;
 let audio = new Audio(songs[songIndex].path)
+let playinLoop = true;
 
+// check loopBtn property
 
+if (playinLoop) {
+    loopBtn.style.color = '#1DB954'
+} else {
+    loopBtn.style.color = 'inherit'
+}
+
+//function to play Songs
 function playSong() {
+    if (playinLoop && songIndex >= songs.length) {
+        songIndex = 0
+    }
     if (audio.played) {
         audio.pause()
     }
-    audio = new Audio(songs[songIndex].path)
+
+    audio = new Audio(songs[songIndex].path);
     audio.play()
+    audio.currentTime = 0
     masterplayInfo()
     playPauseBtn.classList.remove('ri-play-circle-fill')
     playPauseBtn.classList.add('ri-pause-circle-fill')
 }
 
 
+/// handling play pause events
 
 playPauseBtn.addEventListener('click', () => {
     if (audio.paused || audio.currentTime <= 0) {
         audio.play();
         playPauseBtn.classList.remove('ri-play-circle-fill')
         playPauseBtn.classList.add('ri-pause-circle-fill')
-        
+
     } else {
         audio.pause()
         playPauseBtn.classList.remove('ri-pause-circle-fill')
         playPauseBtn.classList.add('ri-play-circle-fill')
     }
 
-    setInterval(() => {
-        seekBar.value = parseInt((audio.currentTime / audio.duration) * 100)
-        if(audio.currentTime === audio.duration){
-            songIndex++
-            audio.currentTime = 0
-            playSong()
-        }
-    }, 1000)
-    
+
 })
+setInterval(() => {
+    //seekbar updation
+    seekBar.value = parseInt((audio.currentTime / audio.duration) * 100)
 
+    if (playinLoop && audio.currentTime === audio.duration) {
+        songIndex++
+        audio.currentTime = 0
+        playSong()
+    }
+}, 1000)
 
+// handle change in seek bar 
 seekBar.addEventListener('change', () => {
     audio.currentTime = seekBar.value * audio.duration / 100
 })
 
+//handling click event on tracks(song) of track List in html and play song  on click
 
 document.querySelectorAll('.track').forEach((track) => {
     track.addEventListener('click', () => {
@@ -132,15 +152,16 @@ document.querySelectorAll('.track').forEach((track) => {
     })
 })
 
+//function to update palyer information
+
 masterplayInfo()
 function masterplayInfo() {
     document.querySelector('#playerCoverImg').src = songs[songIndex].coverPath
     document.querySelector('#playerSongName').textContent = songs[songIndex].name
     document.querySelector('#playerSongDiscription').textContent = songs[songIndex].artist
-    
 }
 
-
+// handling click event on privious btn
 previousBtn.addEventListener('click', () => {
     if (songIndex <= 0) {
         songIndex = songs.length - 1
@@ -150,6 +171,8 @@ previousBtn.addEventListener('click', () => {
     masterplayInfo()
     playSong()
 })
+
+//handling click event in next song btn
 nextBtn.addEventListener('click', () => {
     if (songIndex >= songs.length - 1) {
         songIndex = 0
@@ -159,34 +182,43 @@ nextBtn.addEventListener('click', () => {
     masterplayInfo()
     playSong()
 })
+//handling loop btn events
+loopBtn.addEventListener('click', () => {
+    if (playinLoop) {
+        playinLoop = false
+        loopBtn.style.color = 'inherit'
+    } else {
+        loopBtn.style.color = '#1DB954'
+        playinLoop = true
+    }
+})
 
-playlistplayBtn.addEventListener('click',() =>{
+///handling playlist play btn event
+playlistplayBtn.addEventListener('click', () => {
     songIndex = 0;
     playSong()
     playlistplayBtn.classList.remove('ri-play-circle-fill')
     playlistplayBtn.classList.add('ri-pause-circle-fill')
-    
+
+    playinLoop = true;
+    loopBtn.style.color = '#1DB954'
 })
 
 
-
-
-
-/// volume change
+/// for change volume of audio by volume range input
 const volumeRange = document.querySelector('#volume')
 
-volumeRange.addEventListener('change',()=>{
-    audio.volume = volumeRange.value/100;
-    if(volumeRange.value == 0){
+volumeRange.addEventListener('change', () => {
+    audio.volume = volumeRange.value / 100;
+    if (volumeRange.value == 0) {
         document.querySelector('#volumeIndicator').classList = ''
         document.querySelector('#volumeIndicator').classList.add('ri-volume-mute-fill')
-    }else if(volumeRange.value <= 50){
+    } else if (volumeRange.value <= 50) {
         document.querySelector('#volumeIndicator').classList = ''
         document.querySelector('#volumeIndicator').classList.add('ri-volume-down-fill')
     }
-    else if(volumeRange.value <= 100 ){
+    else if (volumeRange.value <= 100) {
         document.querySelector('#volumeIndicator').classList = ''
         document.querySelector('#volumeIndicator').classList.add('ri-volume-up-fill')
-        
     }
 })
